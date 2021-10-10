a0b4f93d5cbf68e2a3fd612525208bdfeed4cc09
vuyisa01 <218192215@mycput.ac.za>
2021-10-10 22:22:45 +0200

    Fourth Commit

--- src/main/java/za/ac/cput/mentalhealthapp/AboutUs.java
+++ src/main/java/za/ac/cput/mentalhealthapp/AboutUs.java
@@ -1,11 +1,37 @@
 package za.ac.cput.mentalhealthapp;
 
-
 import javax.swing.*;
+import java.awt.*;
 
-public class AboutUs {
 
-    JFrame frame = new JFrame();
-    JLabel lblAboutUs = new JLabel("About Us");
-    JTextArea txtAbout = new JTextArea("All about mental health");
+public class AboutUs extends JFrame {
+
+    private JButton btnBack;
+    private JTextArea ourAimIsToTextArea;
+    private JTextArea takingCareOfYourTextArea;
+    private JLabel lblAbout;
+    private JLabel imgLogo;
+    private JPanel aboutPanel;
+    private JTextArea goToTheSignTextArea;
+    private JLabel lblTut;
+    private JFrame frame;
+
+    public AboutUs(String title){
+            super(title);
+        imgLogo = new JLabel(new ImageIcon("Affirmation.jpg"));
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setContentPane(aboutPanel);
+            this.pack();
+
     }
+    public void setAboutUs(){
+        frame.add(imgLogo);
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new AboutUs("About Us");
+        frame.setVisible(true);
+    }
+
+}
+
