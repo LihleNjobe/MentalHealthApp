87bf5fa13e09fc456b4fe7775685619096a9d9bf
Lindokuhle Nini <lindokuhlepaul6@gmail.com>
2021-10-10 17:11:58 +0200

    Added more folders

--- src/main/java/za/ac/cput/mentalhealthapp/auth/SignIn.java
+++ src/main/java/za/ac/cput/mentalhealthapp/auth/SignIn.java
@@ -1,9 +1,7 @@
-package za.ac.cput.mentalhealthapp;
+package za.ac.cput.mentalhealthapp.auth;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class SignIn extends JFrame {
     private JFrame mainFrame;
